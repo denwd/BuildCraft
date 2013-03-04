809ead0d4d1501ea23b61361dd72a9ea73b4f75d
Agaricus <agaricusb@yahoo.com>
2013-03-03 20:01:49 -0800

    Fix 'type parameter not within bounds' error in ForgeDirection
    EnumSet<E extends Enum<E>>..apparently ForgeDirection doesn't
    extend Enum<E> after it makes its way through the MCPC+ build process..

--- common/buildcraft/transport/EntityData.java
+++ common/buildcraft/transport/EntityData.java
@@ -1,6 +1,7 @@
 package buildcraft.transport;
 
 import java.util.EnumSet;
+import java.util.Set;
 
 import net.minecraftforge.common.ForgeDirection;
 import buildcraft.api.transport.IPipedItem;
@@ -15,7 +16,7 @@ public class EntityData {
 	public ForgeDirection input;
 	public ForgeDirection output = ForgeDirection.UNKNOWN;
 
-	public EnumSet<ForgeDirection> blacklist = EnumSet.noneOf(ForgeDirection.class);
+	public Set<ForgeDirection> blacklist = EnumSet.noneOf(ForgeDirection.class);
 
 	public EntityData(IPipedItem citem, ForgeDirection orientation) {
 		item = citem;
