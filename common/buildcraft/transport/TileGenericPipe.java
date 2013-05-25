c7074248716b34ec9f5378dba0f687a9c2291680
Sirse <sirse.box@gmail.com>
2013-05-25 19:26:10 +0900

    Pipe Chunk Boundary Optimization

--- common/buildcraft/transport/TileGenericPipe.java
+++ common/buildcraft/transport/TileGenericPipe.java
@@ -274,9 +274,7 @@ public class TileGenericPipe extends TileEntity implements IPowerReceptor, ITank
 		for (ForgeDirection o : ForgeDirection.VALID_DIRECTIONS) {
 			Position pos = new Position(xCoord, yCoord, zCoord, o);
 			pos.moveForwards(1.0);
-
-			tileBuffer[o.ordinal()] = new TileBuffer();
-			tileBuffer[o.ordinal()].initialize(worldObj, (int) pos.x, (int) pos.y, (int) pos.z);
+            tileBuffer[o.ordinal()] = new TileBuffer(worldObj, (int) pos.x, (int) pos.y, (int) pos.z, pipe.transport.delveIntoUnloadedChunks());
 		}
 
 		for (ForgeDirection o : ForgeDirection.VALID_DIRECTIONS) {
