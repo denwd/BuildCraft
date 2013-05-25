395c1c67a757430224d91e98fb99bc9c0dae28bf
Sirse <sirse.box@gmail.com>
2013-05-25 19:28:56 +0900

    Remove the builder from the creative tab

--- common/buildcraft/builders/BlockBuilder.java
+++ common/buildcraft/builders/BlockBuilder.java
@@ -41,7 +41,7 @@ public class BlockBuilder extends BlockContainer {
 		blockTextureTop = 3 * 16 + 6;
 		blockTextureFront = 3 * 16 + 7;
 		setHardness(0.7F);
-		setCreativeTab(CreativeTabBuildCraft.tabBuildCraft);
+		//setCreativeTab(CreativeTabBuildCraft.tabBuildCraft);
 	}
 
 	@Override
